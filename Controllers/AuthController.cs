ed0e38d2da67a5c316f98482879818c8e2f649fb
Theo Esberg <esbergtheo@gmail.com>
2025-11-29 14:14:30 +0100

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using Korvan_API.Entities;
+using Korvan_API.Models.DTOs;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Korvan_API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController(IConfiguration configuration) : ControllerBase
+	{
+		public static User user = new();
+
+		[HttpPost("register")]
+		public ActionResult<User> Register(UserDTO request) {
+			var hashedPassword = new PasswordHasher<User>()
+				.HashPassword(user, request.Password);
+
+			user.Username = request.Username;
+			user.PasswordHash = hashedPassword;
+
+			return Ok(user);
+		}
+
+		[HttpPost("login")]
+		public ActionResult<string> Login(UserDTO request)
+		{
+			if (user.Username != request.Username) {
+				return BadRequest("User not found!");
+			}
+
+			if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed) {
+				return BadRequest("Wrong password.");
+			}
+
+			string token = CreateToken(user);
+
+			return Ok(token);
+		}
+
+		private string CreateToken(User user)
+		{
+			var claims = new List<Claim>
+			{
+				new Claim(ClaimTypes.Name, user.Username)
+			};
+
+			var key = new SymmetricSecurityKey(
+				Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+
+			var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+			var tokenDescriptor = new JwtSecurityToken(
+				issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+				audience: configuration.GetValue<string>("AppSettings:Audience"),
+				claims: claims,
+				expires: DateTime.UtcNow.AddDays(1),
+				signingCredentials: creds
+			);
+
+			return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+		}
+	}
+}
---
+++ Data/UserDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Korvan_API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Korvan_API.Data
+{
+	public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
+	{
+		public DbSet<User> Users { get; set; }
+	}
+}
